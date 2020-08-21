48e04bd714ee9066a2c2e17bd8ca9cc219275e84
chitacan <chitacan@gmail.com>
2020-08-21 10:05:32 +0900

    create zgzg.

---
+++ zgzg/README.md
@@ -0,0 +1,30 @@
+# zgzg
+
+personal tunnel for zigzag internal services.
+
+```
+HOST_NAME=
+TARGET_PORT=
+TOKEN=
+```
+
+## run
+
+```
+$ docker-compose --env-file .env build
+$ docker-compose --env-file .env up -d
+```
+
+## development
+
+ngrok
+
+```
+$ ngrok http 4444 --hostname <HOST_NAME> --region jp
+```
+
+mitmproxy
+
+```
+$  mitmproxy -p 4444 --mode reverse:http://127.0.0.1:<TARGET_PORT> -s mitmproxy/allow-cross-site.py
+```
---
+++ zgzg/mitmproxy/allow-cross-site.py
@@ -0,0 +1,45 @@
+from mitmproxy import http
+from mitmproxy.net.http import cookies
+
+def request(flow):
+    if "X-Host" in flow.request.headers:
+        flow.request.host_header = flow.request.headers["X-Host"]
+        flow.request.host = flow.request.headers["X-Host"]
+
+    if "X-Scheme" in flow.request.headers:
+        if flow.request.headers["X-Scheme"] == "https":
+            flow.request.scheme = "https"
+            flow.request.port = 443
+        else:
+            flow.request.scheme = "http"
+            flow.request.port = 80
+
+    if flow.request.method == "OPTIONS":
+        flow.response = http.HTTPResponse.make(
+            200,
+            b"",
+            {
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Method": "GET,POST,OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type,X-Host,X-Scheme"
+            }
+        )
+        return
+
+    flow.request.headers.pop("If-None-Match", None)
+    flow.request.headers.pop("If-Modified-Since", None)
+    flow.request.headers.pop("X-Host", None)
+    flow.request.headers.pop("X-Scheme", None)
+
+def response(flow):
+    if "Origin" in flow.request.headers:
+        flow.response.headers["Access-Control-Allow-Origin"] = flow.request.headers["Origin"]
+    else:
+        flow.response.headers["Access-Control-Allow-Origin"] = "*"
+    if "Set-Cookie" in flow.response.headers:
+        cookie_group = cookies.parse_set_cookie_header(flow.response.headers["Set-Cookie"])
+        for key, val, attrs in cookie_group:
+            if key == 'connect.sid':
+                attrs.add('SameSite', 'None')
+                attrs.add('Secure', '')
+        flow.response.headers["Set-Cookie"] = cookies.format_set_cookie_header(cookie_group)
